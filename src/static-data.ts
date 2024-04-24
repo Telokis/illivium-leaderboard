420f3086cdbe77f13672873c4d9866682388436b
Telokis <6382729+Telokis@users.noreply.github.com>
2024-04-24 09:49:55 +0200

    Update to S4

--- src/static-data.ts
+++ src/static-data.ts
@@ -7,24 +7,16 @@ export const guilds: Array<GuildSpec> = [
 ];
 
 export const dungeons: Array<DungeonSpec> = [
-  { fullName: "Atal'Dazar", shortName: "AD", referenceTime: 30 * 60 },
-  { fullName: "Black Rook Hold", shortName: "BRH", referenceTime: 36 * 60 },
+  { fullName: "Algeth'ar Academy", shortName: "AA", referenceTime: 32 * 60 },
+  { fullName: "Brackenhide Hollow", shortName: "BH", referenceTime: 36 * 60 },
+  { fullName: "Halls of Infusion", shortName: "HOI", referenceTime: 38 * 60 },
+  { fullName: "Neltharus", shortName: "NELT", referenceTime: 35 * 60 },
+  { fullName: "Nokhud Offensive", shortName: "NO", referenceTime: 40 * 60 },
+  { fullName: "Ruby Life Pools", shortName: "RLP", referenceTime: 30 * 60 },
+  { fullName: "The Azure Vault", shortName: "AV", referenceTime: 35 * 60 },
   {
-    fullName: "DOTI: Galakrond's Fall",
-    shortName: "FALL",
-    referenceTime: 34 * 60,
+    fullName: "Uldaman: Legacy of Tyr",
+    shortName: "ULD",
+    referenceTime: 35 * 60,
   },
-  {
-    fullName: "DOTI: Murozond's Rise",
-    shortName: "RISE",
-    referenceTime: 36 * 60,
-  },
-  { fullName: "Darkheart Thicket", shortName: "DHT", referenceTime: 30 * 60 },
-  { fullName: "The Everbloom", shortName: "EB", referenceTime: 33 * 60 },
-  {
-    fullName: "Throne of the Tides",
-    shortName: "TOTT",
-    referenceTime: 34 * 60,
-  },
-  { fullName: "Waycrest Manor", shortName: "WM", referenceTime: 37 * 60 + 0 },
 ];
